98e02ae01063cc380ceebc0f88f479a1dd635a0e
kellerx06 <144833810+kellerx06@users.noreply.github.com>
2023-11-06 11:43:41 -0500

    pause8

--- sketch.js
+++ sketch.js
@@ -24,18 +24,18 @@ function draw() {
   if(paused) {
     //do this stuff
   } else {
-  background(0);
+    background(0);
 
-  translate(width / 2, height / 2);
-  var newzoom = 64 / blob.r;
-  zoom = lerp(zoom, newzoom, 0.1);
-  scale(zoom);
-  translate(-blob.pos.x, -blob.pos.y);
+    translate(width / 2, height / 2);
+    var newzoom = 64 / blob.r;
+    zoom = lerp(zoom, newzoom, 0.1);
+    scale(zoom);
+    translate(-blob.pos.x, -blob.pos.y);
 
-  for (var i = blobs.length - 1; i >= 0; i--) {
-    blobs[i].show();
-    if (blob.eats(blobs[i])) {
-      blobs.splice(i, 1);
+    for (var i = blobs.length - 1; i >= 0; i--) {
+      blobs[i].show();
+      if (blob.eats(blobs[i])) {
+        blobs.splice(i, 1);
     }
   }
 }
