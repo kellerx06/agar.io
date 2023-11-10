c64b59248c40f27e3bbc564cc885f33c70f8a62e
kellerx06 <144833810+kellerx06@users.noreply.github.com>
2023-11-10 08:15:13 -0500

    update

--- sketch.js
+++ sketch.js
@@ -21,38 +21,32 @@ function setup() {
 }
 
 function draw() {
-  if(paused) {
-  //do this stuff
-  } else {
-  background(0);
+    if(paused) {
+    //do this stuff
+    } else {
+    background(0);
 
-  translate(width / 2, height / 2);
-  var newzoom = 64 / blob.r;
-  zoom = lerp(zoom, newzoom, 0.1);
-  scale(zoom);
-  translate(-blob.pos.x, -blob.pos.y);
+    translate(width / 2, height / 2);
+    var newzoom = 64 / blob.r;
+    zoom = lerp(zoom, newzoom, 0.1);
+    scale(zoom);
+    translate(-blob.pos.x, -blob.pos.y);
 
-  for (var i = blobs.length - 1; i >= 0; i--) {
-    blobs[i].show();
-    if (blob.eats(blobs[i])) {
-      blobs.splice(i, 1);
-   }
-  }
-  blob.show();
-  blob.update();
+    for (var i = blobs.length - 1; i >= 0; i--) {
+      blobs[i].show();
+      if (blob.eats(blobs[i])) {
+        blobs.splice(i, 1);
+    }
+    }
+    blob.show();
+    blob.update();
  }
 }
 
   function keyPressed() {
     console.log(key);
-    if(key === 'p') {
-      paused = true
-     }
-  }
-
-  function keyPressed() {
-    console.log(key);
-    if(key === 'p' && paused === true) {
+    if(paused === true && key === 'p') {
       paused = false
-     }
+     } else if {
+      paused = true
 }
