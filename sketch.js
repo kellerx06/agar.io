984c26227cb7847b17441f354646a173dc721f9c
kellerx06 <144833810+kellerx06@users.noreply.github.com>
2023-11-13 11:43:19 -0500

    changecanvas

--- sketch.js
+++ sketch.js
@@ -11,7 +11,7 @@ var zoom = 1;
 var paused = false;
 
 function setup() {
-  createCanvas(1930, 950);
+  createCanvas(1900, 1000);
   blob = new Blob(0, 0, 64);
   for (var i = 0; i < 1000; i++) {
     var x = random(-width, width);
