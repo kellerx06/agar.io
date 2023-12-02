441b351c2392d9553487c8b3f452b2b927958ccf
kellerx06 <144833810+kellerx06@users.noreply.github.com>
2023-12-02 00:56:15 -0500

    reset

--- sketch.js
+++ sketch.js
@@ -9,63 +9,74 @@ var zoom = 1;
 
 var flashingColors = false;
 
+let counter = 0;
+let myColor;
+
 var paused = false;
 
 var score = 0;
 
 function setup() {
   createCanvas(1900, 1000);
+  myColor = color(random(255), random(255), random(255));
   frameRate(30);
   blob = new Blob(0, 0, 64);
+  fill(myColor);
   for (var i = 0; i < 1000; i++) {
     var x = random(-width, width);
     var y = random(-height, height);
     blobs[i] = new Blob(x, y, 16);
   }
+  if (counter > 19) {
+    myColor = color(random(255), random(255), random(255));
+    counter = 0;
+  }
+  counter = counter + 1;
 }
 
 function draw() {
   if (paused) {
-    displayPausedScreen();
+    textSize(100);
+    fill(255, 0, 0);
+    textAlign(CENTER, CENTER);
+    const middleX = width / 2;
+    const middleY = height / 2;
+    text("PAUSED", middleX, middleY);
+    console.log("PAUSED");
+
+    fill(255);
+    rect(840, 700, 200, 75);
+    fill(0);
+    textSize(50);
+    text("RESET", 940, 745);
+    pop();
   } else {
     background(0);
-    updateBlob();
-    drawBlobs();
-    drawScore();
-  }
+    fill(255, 255, 255);
+    translate(width / 2, height / 2);
+    var newzoom = 64 / blob.r;
+    zoom = lerp(zoom, newzoom, 0.1);
+    scale(zoom);
+    translate(-blob.pos.x, -blob.pos.y);
 
-  if (flashingColors) {
-    flashColors();
-  }
-}
-
-function updateBlob() {
-  fill(255, 255, 255);
-  translate(width / 2, height / 2);
-  var newzoom = 64 / blob.r;
-  zoom = lerp(zoom, newzoom, 0.1);
-  scale(zoom);
-  translate(-blob.pos.x, -blob.pos.y);
-  blob.show();
-  blob.update();
-  resetMatrix();
-}
-
-function drawBlobs() {
-  for (var i = blobs.length - 1; i >= 0; i--) {
-    blobs[i].show();
-    if (blob.eats(blobs[i])) {
-      blobs.splice(i, 1);
-      score++;
+    for (var i = blobs.length - 1; i >= 0; i--) {
+      blobs[i].show();
+      if (blob.eats(blobs[i])) {
+        blobs.splice(i, 1);
+        score++;
+      }
     }
+
+    blob.show();
+    blob.update();
+
+    resetMatrix();
+    textSize(32);
+    fill(255);
+    text("Score: " + score, 50, 50);
   }
 }
 
-function drawScore() {
-  textSize(32);
-  fill(255);
-  text("Score: " + score, 50, 50);
-}
 
 function keyPressed() {
   if (key === "p") {
@@ -73,36 +84,13 @@ function keyPressed() {
   } else if (key === "c") {
     flashingColors = !flashingColors;
   }
-}
+  
 
-function flashColors() {
-  for (var i = 0; i < blobs.length; i++) {
-    blobs[i].flash();
-  }
-}
 
-if (!Blob.prototype.flash) {
-  Blob.prototype.flash = function () {
-    this.color = color(random(255), random(255), random(255));
-  };
-}
-
-function displayPausedScreen() {
-  textSize(100);
-  fill(255, 0, 0);
-  textAlign(CENTER, CENTER);
-  const middleX = width / 2;
-  const middleY = height / 2;
-  text("PAUSED", middleX, middleY);
-  console.log("PAUSED");
-
-  fill(255);
-  rect(840, 700, 200, 75);
-  fill(0);
-  textSize(50);
-  text("RESET", 940, 745);
-  pop();
-}
+//function mouseMoved() {
+ // console.log(`${mouseX}, ${mouseY}`);
+  //return false;
+//}
 
 function resetGame() {
   blob = new Blob(0, 0, 64);
@@ -116,8 +104,16 @@ function resetGame() {
 }
 
 function mousePressed() {
+  // Check if the mouse is within the boundaries of the reset button
   if (mouseX > 840 && mouseX < 1040 && mouseY > 700 && mouseY < 775) {
-    paused = false;
+    // Reset the game
+    paused = false; // Unpause the game
     resetGame();
+  }
+}
+
+function flashColors() {
+  for (var i = 0; i < blobs.length; i++) {
+    blobs[i].flash();
   }
 }
