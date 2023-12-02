f198a8a7587dfc481f9b8ee4e5204acd92befb38
kellerx06 <144833810+kellerx06@users.noreply.github.com>
2023-12-01 19:42:54 -0500

    fix

--- blob.js
+++ blob.js
@@ -1,5 +1,5 @@
 // Daniel Shiffman
-// http://codingtra.in
+// http://codingtra.in/
 // http://patreon.com/codingtrain
 // Code for: https://youtu.be/JXuxYMGe4KI
 
@@ -7,16 +7,16 @@ function Blob(x, y, r) {
   this.pos = createVector(x, y);
   this.r = r;
   this.vel = createVector(0, 0);
-  this.col =(random(255), random(100), random(255));
+  this.col = color(random(255), random(255), random(255));
 
-  this.update = function() {
+  this.update = function () {
     var newvel = createVector(mouseX - width / 2, mouseY - height / 2);
     newvel.setMag(3);
     this.vel.lerp(newvel, 0.2);
     this.pos.add(this.vel);
-  }
+  };
 
-  this.eats = function(other) {
+  this.eats = function (other) {
     var d = p5.Vector.dist(this.pos, other.pos);
     if (d < this.r + other.r) {
       var sum = PI * this.r * this.r + PI * other.r * other.r;
@@ -26,10 +26,11 @@ function Blob(x, y, r) {
     } else {
       return false;
     }
-  }
+  };
 
-  this.show = function() {
+  this.show = function () {
+    //fill(0, 255, 0);
     fill(this.col);
     ellipse(this.pos.x, this.pos.y, this.r * 2, this.r * 2);
-  }
+  };
 }
